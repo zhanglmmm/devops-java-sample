c9c0dec10b87596edcb9abc4803ddebcdf789c84
张黎明 <zhangliming@fangyibao.cn>
2020-11-20 17:03:42 +0800

    update

--- src/main/java/io/kubesphere/devops/Application.java
+++ src/main/java/io/kubesphere/devops/Application.java
@@ -3,9 +3,13 @@ package io.kubesphere.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* @author zhanglm
+* @version 2020年11月20日 下午5:03:27
+* 类说明
+*/
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
